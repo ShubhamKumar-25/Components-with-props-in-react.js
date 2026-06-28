1f4233799d3bf5d7fd593f42227611992b82c5c4
Shubham <sk5989229@gmail.com>
2026-06-28 13:34:13 +0530

    Css file add for liked button

--- src/components/Liked.jsx
+++ src/components/Liked.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import "./Liked.css";
 
 const Liked = () => {
   const [like, setLike] = useState("false");
 
   return (
     <div className="Container">
+      <h1 className="heading">This is my like Button </h1>
       <h2 id="head">{like ? "❤️ Like" : "🤍 dislike"}</h2>
 
       <button onClick={() => setLike(!like)}>click</button>
