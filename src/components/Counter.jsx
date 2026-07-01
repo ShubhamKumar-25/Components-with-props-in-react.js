58e20e43730e9d6102cba5197a96d56bcfa169bc
Shubham <sk5989229@gmail.com>
2026-07-01 18:20:44 +0530

    Decerement function change

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,31 +4,31 @@ import "./Counter.css";
 const Counter = () => {
   const [count, setCount] = useState(0);
 
+  const handleIncrease = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrease = () => {
+    if (count === 0) {
+      return; // Prevent count from going below 0
+    }
+    setCount(count - 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
   return (
     <div className="counter-container">
       <h1>Counter : {count}</h1>
-      <button
-        className="btn"
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
+      <button className="btn" onClick={handleIncrease}>
         Increase
       </button>
-      <button
-        className="btn2"
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
+      <button className="btn2" onClick={handleDecrease}>
         Decrease
       </button>
-      <button
-        className="btn3"
-        onClick={() => {
-          setCount(0);
-        }}
-      >
+      <button className="btn3" onClick={handleReset}>
         Reset
       </button>
     </div>
